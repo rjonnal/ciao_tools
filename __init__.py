39de051348c347d137737b7684d037eb8c5fbe19
Ravi Sankar Jonnal <rjonnal@gmail.com>
2020-10-23 14:32:55 -0700

    initial commit

---
+++ __init__.py
@@ -0,0 +1,10 @@
+from __future__ import print_function, division
+import numpy as np
+import mraw_reader
+from matplotlib import pyplot as plt
+import os,sys.glob
+
+class Log:
+
+    def __init__(self,log_directory):
+        pass
